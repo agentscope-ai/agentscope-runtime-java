02392ed84f62adb2e03d7f088a09e1e449769413
xuehuitian45 <13069167198@163.com>
2025-12-11 14:38:24 +0800

    fix: fix licenses

--- web/src/main/java/io/agentscope/runtime/protocol/responseapi/model/ResponseApiRequest.java
+++ web/src/main/java/io/agentscope/runtime/protocol/responseapi/model/ResponseApiRequest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.agentscope.runtime.protocol.responseapi.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
