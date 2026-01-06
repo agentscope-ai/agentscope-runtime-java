2845b9fd35ef2746dee46e85ac756c375e5367dd
xuehuitian45 <13069167198@163.com>
2026-01-06 17:27:55 +0800

    fix: add missing licenses

--- sandbox-core/src/main/java/io/agentscope/runtime/sandbox/manager/utils/SandboxMap.java
+++ sandbox-core/src/main/java/io/agentscope/runtime/sandbox/manager/utils/SandboxMap.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.agentscope.runtime.sandbox.manager.utils;
 
 import io.agentscope.runtime.sandbox.manager.model.container.ContainerModel;
@@ -6,19 +22,19 @@ import io.agentscope.runtime.sandbox.manager.model.container.SandboxKey;
 import java.util.Map;
 
 public interface SandboxMap {
-    public void addSandbox(SandboxKey sandboxKey, ContainerModel containerModel);
+    void addSandbox(SandboxKey sandboxKey, ContainerModel containerModel);
 
-    public ContainerModel getSandbox(SandboxKey sandboxKey);
+    ContainerModel getSandbox(SandboxKey sandboxKey);
 
-    public boolean removeSandbox(SandboxKey sandboxKey);
+    boolean removeSandbox(SandboxKey sandboxKey);
 
-    public ContainerModel getSandbox(String containerId);
+    ContainerModel getSandbox(String containerId);
 
-    public void removeSandbox(String containerId);
+    void removeSandbox(String containerId);
 
-    public Map<String, ContainerModel> getAllSandboxes();
+    Map<String, ContainerModel> getAllSandboxes();
 
-    public boolean containSandbox(SandboxKey sandboxKey);
+    boolean containSandbox(SandboxKey sandboxKey);
 
-    public boolean containSandbox(String containerId);
+    boolean containSandbox(String containerId);
 }
